fde786f6c3ff9690b5497a305c4ac94133327f82
aiharafat <ibnehasanabdulllah@gmail.com>
2024-10-22 19:23:33 +0600

    k

--- src/components/ui/pages/News.jsx
+++ src/components/ui/pages/News.jsx
@@ -15,28 +15,29 @@ const News = () => {
 
           {/* YouTube News Button */}
           <div className="flex items-center justify-between px-[50px] mt-[50px]">
-          <Link to="/News/YouTube" className="text-center">
-              <div className="relative bg-[#99bfb6] w-24 h-24 flex justify-center items-center rounded-full shadow-lg hover:bg-[#7fa49d] transition-transform duration-300 transform hover:scale-110">
-                <img
-                  src={youtubeIcon}
-                  alt="YouTube Icon"
-                  className="w-8 h-8 transition-transform duration-300"
-                />
-              </div>
-              <span className="text-sm text-white mt-2 block">YouTube </span>
-            </Link>
-           
-            <Link to="/News/Website" className="text-center">
-              <div className="relative bg-[#99bfb6] w-24 h-24 flex justify-center items-center rounded-full shadow-lg hover:bg-[#7fa49d] transition-transform duration-300 transform hover:scale-110">
-                <img
-                  src={websiteIcon}
-                  alt="Website Icon"
-                  className="w-[60px] h-[60px] transition-transform duration-300"
-                />
-              </div>
-              <span className="text-sm text-white mt-2 block">Website </span>
-            </Link>
-          </div>
+  <Link to="/News/YouTube" className="text-center">
+    <div className="relative bg-[#99bfb6] w-24 h-24 flex justify-center items-center rounded-full shadow-lg hover:bg-[#7fa49d] transition-transform duration-300 transform hover:scale-110">
+      <img
+        src={youtubeIcon}
+        alt="YouTube Icon"
+        className="w-8 h-8 transition-transform duration-300"
+      />
+    </div>
+    <span className="text-sm text-white mt-2 block">YouTube </span>
+  </Link>
+  
+  <Link to="/News/Website" className="text-center">
+    <div className="relative bg-[#99bfb6] w-24 h-24 flex justify-center items-center rounded-full shadow-lg hover:bg-[#7fa49d] transition-transform duration-300 transform hover:scale-110">
+      <img
+        src="/assets/Website.png"
+        alt="Website Icon"
+        className="w-[60px] h-[60px] transition-transform duration-300"
+      />
+    </div>
+    <span className="text-sm text-white mt-2 block">Website</span>
+  </Link>
+</div>
+
 
           
 
